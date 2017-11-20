49d39e440fe0bf2896ce88da5a8bbe238e161c8a
ZHAOHUHU <3066918701@qq.com>
2017-11-20 18:25:39 +0800

    ok

--- src/main/java/teamway/shenzhen/tms9000/DBServer.java
+++ src/main/java/teamway/shenzhen/tms9000/DBServer.java
@@ -2,25 +2,40 @@ package teamway.shenzhen.tms9000;
 
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.server.TNonblockingServer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TTransportException;
 
 public class DBServer {
 
 	private static final int port = 9996;
 	//private static final int clientTimeout = null;
 	@SuppressWarnings("resource")
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		System.out.println("action.......");
-		TProcessor tp = new DbService.Processor<DbService.Iface>(new DBimp());
-		TServerSocket socket = new TServerSocket(port);
-		TThreadPoolServer.Args thb_args = new TThreadPoolServer.Args(socket);
-		thb_args.processor(tp);
-		thb_args.protocolFactory(new TBinaryProtocol.Factory());
-		TServer server =new TThreadPoolServer(thb_args);
-		
-		System.out.println("端口数："+port);
-		server.serve();
+		  try {
+	            TNonblockingServerSocket serverTransport = new TNonblockingServerSocket(port);
+	            // 设置协议工厂为 TBinaryProtocol.Factory
+	            // 关联处理器与 Hello 服务的实现
+	            TProcessor tprocessor = new DbService.Processor(new DBimp());
+	            //异步IO，需要使用TFramedTransport，它将分块缓存读取。
+	            TNonblockingServer.Args tArgs = new TNonblockingServer.Args(serverTransport);
+	            tArgs.processor(tprocessor);
+	            tArgs.transportFactory(new TFramedTransport.Factory());
+	            //使用二进制协议
+	            tArgs.protocolFactory(new TBinaryProtocol.Factory());
+	            //线程池服务模型，使用标准的阻塞式IO，预先创建一组线程处理请求。
+	            TServer server =new TNonblockingServer(tArgs);
+	            System.out.println("Start server on "+port);
+	            server.serve();
+	        } catch (TTransportException e) {
+	            e.printStackTrace();
+	        }
+	    }
 	}
-}
+
