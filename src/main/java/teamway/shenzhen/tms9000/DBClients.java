f5249863399fa0c40d9fd9b9aa18f9ac88456585
ZHAOHUHU <3066918701@qq.com>
2017-11-20 18:42:05 +0800

    微笑的改动

--- src/main/java/teamway/shenzhen/tms9000/DBClients.java
+++ src/main/java/teamway/shenzhen/tms9000/DBClients.java
@@ -15,34 +15,7 @@ public class DBClients {
 	private static String host = "127.0.0.1";
 
 	public static void main(String[] args) {
-//		try {
-//			// 设置调用的服务地址为本地，端口为 7911 // 设置传输通道
-//			TTransport transport = new TSocket(host, port);
-//			transport.open();
-//			// 设置传输协议为 TBinaryProtocol
-//			/*
-//			 * 有错误再说 TSocket transport = new TSocket("localhost", 9090); transport.open();
-//			 * TProtocol protocol = new TBinaryProtocol(new
-//			 * TFastFramedTransport(transport));
-//			 */
-//			TProtocol protocol = new TBinaryProtocol(transport);
-//			DbService.Client client = new DbService.Client(protocol);
-//
-//			client.initDB("127.0.0.1", 3306, "root", "74110", "test", 3, 10);
-//			// System.out.println(b);
-//			// System.out.println(c);
-//			//String sql = "INSERT INTO huhu (id,girlname)VALUES(96,'wodetianan怎么会这样')";
-//		//	client.executeNoneQuery(sql);
-//			String sql2 = "select * from huhu";
-//			String string = client.queryObject(sql2);
-//			System.out.println(string);
-//
-//			transport.close();
-//		} catch (TTransportException e) {
-//			e.printStackTrace();
-//		} catch (TException e) {
-//			e.printStackTrace();
-//		}
+//		
 		 try {
 	           //设置传输通道，对于非阻塞服务，需要使用TFramedTransport，它将数据分块发送
 	            TTransport transport = new TFramedTransport(new TSocket("localhost", port,3000));
